7abdde5b481b0e6d49640a1d618b6b26c3dc47be
Alex Cote <apc51104@gmail.com>
2023-09-08 15:39:01 -0400

    adding dropdown

--- constitution/resources/script.js
+++ constitution/resources/script.js
@@ -1,3 +1,10 @@
 $(document).ready( function() {
 
+   $("a#hideText").click(function () {
+      $("#dropdown p").hide(400);
+   });
+   $("a#showText").click(function () {
+      $("#dropdown p").show(2000);
+   });
+
 });
