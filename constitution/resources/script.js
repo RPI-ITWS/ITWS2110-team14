191dd300dffc9230033410f4b7be0d78e2920a44
Alex Cote <apc51104@gmail.com>
2023-09-11 19:23:06 -0400

    fixing dropdown stuff

--- constitution/resources/script.js
+++ constitution/resources/script.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
-  $("a#hideText").click(function () {
-    $(".dropdown p").hide(400);
+  $("a.hideText").click(function () {
+    $(".textContainer p").hide(400);
   });
-  $("a#showText").click(function () {
-    $(".dropdown p").show(400);
+  $("a.showText").click(function () {
+    $(".textContainer p").show(400);
   });
 });
