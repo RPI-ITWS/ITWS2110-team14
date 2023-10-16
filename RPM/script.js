142188ba379f9687e973635ddac010f64e655996
patela16 <122549881+patela16@users.noreply.github.com>
2023-10-16 01:22:00 -0400

    Create background animation

--- RPM/script.js
+++ RPM/script.js
@@ -1,14 +1,42 @@
-// button-animation.js
+// script.js
 
 const card = document.getElementById('card');
 const loginButton = document.getElementById('loginButton');
 const rightHand2 = document.getElementById('rightHand2');
 const leftHands = document.getElementById('leftHands');
 const rightHand1 = document.getElementById('rightHand1');
+const bgContainer = document.querySelector(".animated-bg");
 
-// Animation on card hover
+// Background animation
+function createRectangle(left, initial = false) {
+  const rect = document.createElement("div");
+
+  rect.classList.add("moving-rectangle");
+  rect.style.top = `${Math.random() * 100}vh`;
+  rect.style.left = `${left}vh`;
+  rect.style.backgroundColor = `rgba(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255}, 0.1)`;
+  rect.style.width = `${Math.random() * 200 + 25}px`;
+  rect.style.height = `${Math.random() * 5 + 20}px`;
+  rect.style.animationDuration = `${Math.random() * 10 + 10}s`;
+  rect.style.opacity = `0`;
+
+  bgContainer.appendChild(rect);
+  rect.addEventListener("animationend", () => {
+    rect.remove();
+  });
+}
+
+// Shadow on card hover
 card.addEventListener('mouseenter', () => {
   card.style.boxShadow = '0px 6px 20px rgba(0, 0, 0, 0.2)';
+});
+
+card.addEventListener('mouseleave', () => {
+  card.style.boxShadow = 'none';
+});
+
+// Animation on button hover
+loginButton.addEventListener('mouseenter', () => {
   rightHand2.style.transform = 'rotate(-10deg)';
   rightHand2.style.left = '60.5%';
   leftHands.style.transform = 'rotate(10deg)';
@@ -17,8 +45,7 @@ card.addEventListener('mouseenter', () => {
   rightHand1.style.left = '59%';
 });
 
-card.addEventListener('mouseleave', () => {
-  card.style.boxShadow = 'none';
+loginButton.addEventListener('mouseleave', () => {
   rightHand2.style.transform = 'rotate(0deg)';
   rightHand2.style.left = '61%';
   leftHands.style.transform = 'rotate(0deg)';
@@ -51,12 +78,26 @@ document.getElementById('loginButton').addEventListener('click', function (func)
   createRippleOnButton(func, this);
 });
 
-document.getElementById('loginButtonImg').addEventListener('click', function (func) {
-  const loginButton = document.getElementById('loginButton');
-  createRippleOnButton(func, loginButton);
-});
-
 // Login action
 loginButton.addEventListener('click', () => {
   console.log("Login clicked")
 });
+
+// Continuous actions
+for (let i = 0; i < 200; i++) {
+  createRectangle(Math.random() * 250 - 50, true);
+}
+
+setInterval(() => createRectangle(-50), 100);
+
+// On page load
+window.addEventListener('load', function() {
+  const card = document.getElementById('card');
+  if (card) {
+    card.style.opacity = "1";
+  }
+});
+
+window.addEventListener('wheel', function(e) {
+    e.preventDefault();
+}, { passive: false });
