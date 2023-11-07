cedb2956fc516824af6504566eef7507a42db73a
patela16 <122549881+patela16@users.noreply.github.com>
2023-11-07 15:02:14 -0500

    Visibility fixes, added text on home page

--- lab6/content/script.js
+++ lab6/content/script.js
@@ -11,49 +11,25 @@ window.addEventListener('load', function() {
   .then(courses => {
     let lectures_list = document.getElementById("items-lectures");
     let labs_list = document.getElementById("items-labs");
-        
+
     let lectures_json = courses["Websys_course"][0]["Lectures"];
     let labs_json = courses["Websys_course"][0]["Labs"];
 
-    let i = 1;
-    for (let lectureKey in lectures_json) {
-      let new_element = document.createElement("li");
-
-      new_element.innerHTML = "<a href='#'>Lecture " + i + "</p>";
-      new_element.id = "lecture" + i;
-
-      new_element.addEventListener("click", function() {
-        lectureClicked(lectures_json[lectureKey]);
-      });
-
-      lectures_list.appendChild(new_element);
-      i += 1;
-    }
-
-    i = 1;
-    for (let labKey in labs_json) {
-      let new_element = document.createElement("li");
-
-      new_element.innerHTML = "<a href='#'>Lab " + i + "</p>";
-      new_element.id = "lab" + i;
-
-      new_element.addEventListener("click", function() {
-        labClicked(labs_json[labKey]);
-      });
-
-      labs_list.appendChild(new_element);
-      i += 1;
-    }
-
+    createListItems(lectures_json, lectures_list, "Lecture", lectureClicked);
     lectures_list.style.visibility = "visible";
+
+    createListItems(labs_json, labs_list, "Lab", labClicked);
     labs_list.style.visibility = "visible";
+    
+    document.getElementById("content-data").style.visibility = 'visible';
+    document.getElementById("content-divider").style.display = 'block';
   });
 });
 
 function lectureClicked(lecture) {
   let content = document.getElementById("content-data");
   content.innerHTML = "<h2>" + lecture.title + "</h2>";
-  content.innerHTML += lecture.description;
+  content.innerHTML += "<p>" + lecture.description + "</p>";
 
   let links = content.links;
 }
@@ -61,5 +37,27 @@ function lectureClicked(lecture) {
 function labClicked(lab) {
   let content = document.getElementById("content-data");
   content.innerHTML = "<h2>" + lab.title + "</h2>";
-  content.innerHTML += lab.description;
+  content.innerHTML += "<p>" + lab.description + "</p>";
+}
+
+function createListItems(itemJson, itemList, itemPrefix, clickHandler) {
+  let i = 1;
+  for (let itemKey in itemJson) {
+    let new_element = document.createElement("li");
+    let link = document.createElement("a");
+
+    link.href = '#';
+    link.textContent = itemPrefix + " " + i;
+    link.addEventListener("click", (function(key) {
+      return function() {
+        clickHandler(itemJson[key]);
+      }
+    })(itemKey));
+
+    new_element.appendChild(link);
+    new_element.id = itemPrefix.toLowerCase() + i;
+
+    itemList.appendChild(new_element);
+    i += 1;
+  }
 }
