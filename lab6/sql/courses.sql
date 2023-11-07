751d4b3b0411ba3600d3d0659d16b13bbd416559
millen11 <millen11@rpi.edu>
2023-11-07 14:39:58 -0500

    millen11-step4
    Inserted data into courses table

--- lab6/sql/courses.sql
+++ lab6/sql/courses.sql
@@ -8,3 +8,15 @@ CREATE TABLE IF NOT EXISTS courses {
 ALTER TABLE courses
   ADD COLUMN section INT(2),
   ADD COLUMN yyyy INT(4);
+
+INSERT INTO courses(crn, prefix, num, title) --Web Sys
+  VALUES (63146, "ITWS", 2010, "Web-Systems Development");
+
+INSERT INTO courses(crn, prefix, num, title) --Comp org
+  VALUES (63847, "CSCI", 2500, "Computer Organization");
+
+INSERT INTO courses(crn, prefix, num, title) --FOCS
+  VALUES (62129, "CSCI", 2200, "Foundations of Computer Science");
+
+INSERT INTO courses(crn, prefix, num, title) --Multi
+  VALUES (61471, "MATH", 2010, "Multivariable Calculus & Matrix Algebra");
