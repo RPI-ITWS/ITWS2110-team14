9080b1f1d56a50955a2a5abb630425aebbac5aec
millen11 <millen11@rpi.edu>
2023-11-10 09:01:54 -0500

    Update selectors.sql

--- lab6/sql/selectors.sql
+++ lab6/sql/selectors.sql
@@ -2,8 +2,16 @@
 SELECT * FROM students ORDER BY rin ASC, lastname ASC, rcsid ASC, firstname ASC;
 
 --Step 8
-
+SELECT students.rin, CONCAT(students.firstname, ' ', students.lastname) AS studentname, students.street, students.city,
+  students.stateabbr, students.zip FROM students JOIN grades ON students.rin = grades.rin
+  WHERE grades.grade > 90;
 
 --Step 9
+SELECT courses.crn, courses.title, AVG(grades.grade) AS averagegrade FROM courses
+  JOIN grades ON courses.crn = grades.crn
+  GROUP BY courses.crn, courses.title;
 
 --Step 10
+SELECT courses.crn, courses.title, COUNT(DISTINCT students.rin) AS totalstudents FROM courses
+  JOIN grades ON courses.crn = grades.crn JOIN students ON grades.rin = students.rin
+  GROUP BY courses.crn, courses.title;
