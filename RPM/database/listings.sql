56c3eeaecf4bee068fc59bf6624545851356d204
millen11 <millen11@rpi.edu>
2023-11-25 19:46:59 -0500

    millen11-refactoredDBstructure
    1. Made rcs_id the primary key in users
    2. Removed active/past listings in the listing table
    3. Made a new table that stores all listings
    4. Added a listing id to the listings table

--- RPM/database/listings.sql
+++ RPM/database/listings.sql
@@ -1,11 +1,12 @@
-CREATE TABLE LISTINGS (
+CREATE TABLE listings (
+  listing_id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
   rcs_id VARCHAR(10) NOT NULL,
-  active BOOLEAN NOT NULL,
-  color VARCHAR (30) NOT NULL,
   item_condition VARCHAR(12) NOT NULL,
-  clicks INT UNSIGNED,
   item_description VARCHAR(255) NOT NULL, 
   listing_title VARCHAR(100) NOT NULL,
   posting_date DATE NOT NULL,
-  price DECIMAL(10,2) NOT NULL
+  price DECIMAL(10,2) NOT NULL,
+  active BOOLEAN NOT NULL,
+  color VARCHAR (30) NOT NULL,
+  clicks INT UNSIGNED
 );
