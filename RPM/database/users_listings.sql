643fd4bee6d633f96c6b25e81ff43062c2a7da68
Alex Cote <apc51104@gmail.com>
2023-11-30 17:07:04 -0500

    Add image_path column to listings table and modify price column data type

--- RPM/database/users_listings.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE users_listings (
-  rcs_id VARCHAR(10) NOT NULL,
-  listing_id INT NOT NULL,
-  PRIMARY KEY (rcs_id, listing_id),
-  CONSTRAINT FK_rcs_id FOREIGN KEY (rcs_id) REFERENCES users(rcs_id),
-  CONSTRAINT FK_listing_id FOREIGN KEY (listing_id) REFERENCES listings(listing_id)
-);
