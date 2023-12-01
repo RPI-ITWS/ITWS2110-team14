a39f6c4596d46f5d5b2db06e89faa98f983fad97
millen11 <millen11@rpi.edu>
2023-12-01 02:00:56 -0500

    millen11-updatedRemoveListingPHP
    Changed from mysqli to PDO
    Cleaned up code

--- RPM/pages/php/remove_listing.php
+++ RPM/pages/php/remove_listing.php
@@ -1,35 +1,29 @@
 <?php
 /*The following code removes a listing from the marketplace by changing its active status to FALSE*/
-//DB Credentials
-$host = "localhost";
-$dbName = "rpm";
-$username = 'root';
-$password = 'M4k3t14!';
 
+// Connect to the database
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 session_start();
+include_once("../../database/connect.php");
 
-if (isset($_POST["submit"]) && isset($_SESSION["rcs_id"])) { //Get repsonse from remove listing button
-  $updatedStatus = FALSE;
-  $rcs_id = $_SESSION["rcs_id"];
-  $listing_id = $_POST["listing_id"];
-  $dbIsConnected = mysqli_connect($host, $username, $password, $dbName);
-  if (!empty($listingTitle)) {
-    if (!$dbIsConnected) { //Was not able to establish connection to rpm
-      die(mysqli_connect_error());
+try {
+  if (isset($_POST["submit"]) && isset($_SESSION["rcs_id"])) { //Get repsonse from remove listing button
+    $updatedStatus = FALSE;
+    $rcs_id = $_SESSION["rcs_id"];
+    $listing_id = $_POST["listing_id"];
+    $dbIsConnected = mysqli_connect($host, $username, $password, $dbName);
+    if (!empty($listingTitle)) {
+      $updateStatus = $pdo->prepare("UPDATE listings SET active = '$updatedStatus' WHERE listing_id = '$listing_id'");
+      $updateStatus->bindParam(':updatedStatus', $updatedStatus);
+      $updateStatus->execute();
+      echo "Listing removed";
+      header("Location: ../pages/marketplace/index.html");
+      exit();
     } 
-    else { //Connection has been established to rpm
-      $updateStatus = "UPDATE listings SET active = '$updatedStatus' WHERE listing_id = '$listing_id'";
-      if (mysqli_query($dbIsConnected, $updateStatus)) { //SQL Statement was successfully executed
-        echo "Listing removed";
-        header("Location: ../pages/marketplace/index.html");
-        exit();
-      }
-      else { //Error occurred while executing query
-        echo "Could not remove listing. Try again!";
-      }
-    }
   }
-  else {
-    echo "Could not find listing!";
-  }
+}
+
+catch (PDOException $e) { //Issue occured
+  echo "Connection failed: " . $e->getMessage();
 }
