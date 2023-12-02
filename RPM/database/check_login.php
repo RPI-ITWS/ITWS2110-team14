1bc8ac8c8affef720ea86644e26ef1b44afc961b
Alex Cote <apc51104@gmail.com>
2023-12-01 19:38:07 -0500

    Fix session start issue in check_login.php

--- RPM/database/check_login.php
+++ RPM/database/check_login.php
@@ -1,8 +1,8 @@
 <?php
 
-  error_reporting(E_ALL);
-  ini_set('display_errors', 1);
-  session_start();
+  if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+  }
   
   if(isset($_SESSION["rcs_id"])) {
     header("Location: /RPM/pages/user/login");
