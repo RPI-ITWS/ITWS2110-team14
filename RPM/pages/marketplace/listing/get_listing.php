9a2f30973ae47a1ffe6ad815189d5e0ac8b6a687
Alex Cote <apc51104@gmail.com>
2023-12-01 19:21:56 -0500

    Refactor fetch requests and fix file path

---
+++ RPM/pages/marketplace/listing/get_listing.php
@@ -0,0 +1,20 @@
+<?php
+
+  error_reporting(E_ALL);
+  ini_set('display_errors', 1);
+  session_start();
+  include_once("../../database/connect.php");
+  include_once("../../database/check_login.php");
+
+  if(isset($_POST['id'])) {
+    $stmt = $pdo->prepare("SELECT * FROM listings WHERE listing_id = :id");
+    $stmt->execute(array(':id' => $_POST['id']));
+    $listing = $stmt->fetch(PDO::FETCH_ASSOC);
+    $listing = json_encode($listing);
+    echo $listing;
+  }
+  else {
+    echo "No ID";
+  }
+
+?>
--- RPM/pages/marketplace/listing/script.js
+++ RPM/pages/marketplace/listing/script.js
@@ -9,21 +9,28 @@ document.addEventListener("DOMContentLoaded", function() {
     window.location.href = "/RPM/pages/marketplace/";
   }
 
-  fetch('../items.json')
-    .then(response => response.json())
-    .then(data => {
-      const listingData = data.listings[id];
-      
-      console.log(listingData);
+  fetch('get_listing.php', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: `id=${encodeURIComponent(id)}`,
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log(data)
+    const listingData = data.listings[id];
+    
+    console.log(listingData);
 
-      document.querySelector('.image img').src = listingData.imgURL;
-      document.getElementById('listingTitle').textContent = listingData.LISTING_TITLE;
-      document.getElementById('condition').textContent = listingData.CONDITION;
-      document.getElementById('price').textContent = "$" + listingData.PRICE.toFixed(2);
-      document.getElementById('listingInfo').textContent = listingData.ITEM_DESCRIPTION;
+    document.querySelector('.image img').src = listingData.imgURL;
+    document.getElementById('listingTitle').textContent = listingData.LISTING_TITLE;
+    document.getElementById('condition').textContent = listingData.CONDITION;
+    document.getElementById('price').textContent = "$" + listingData.PRICE.toFixed(2);
+    document.getElementById('listingInfo').textContent = listingData.ITEM_DESCRIPTION;
     })
-    .catch(error => {
-      console.error("Error fetching the JSON:", error);
-      window.location.href = "/RPM/pages/marketplace/";
-    });
+  .catch(error => {
+    console.error("Error fetching the JSON:", error);
+    window.location.href = "/RPM/pages/marketplace/";
+  });
 });
