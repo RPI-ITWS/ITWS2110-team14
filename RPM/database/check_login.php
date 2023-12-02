d9063570c8df48c4c2d7a3fa74c09f8d5988adad
Alex Cote <apc51104@gmail.com>
2023-12-01 19:34:05 -0500

    Fix session check in check_login.php

--- RPM/database/check_login.php
+++ RPM/database/check_login.php
@@ -1,10 +1,9 @@
 <?php
 
-  error_reporting(E_ALL);
-  ini_set('display_errors', 1);
+  
   session_start();
   
-  if(isset($_SESSION["rcs_id"])) {
+  if(!isset($_SESSION["rcs_id"])) {
     header("Location: /RPM/pages/user/login");
   }
 
