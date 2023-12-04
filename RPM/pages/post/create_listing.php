8c4f83f70dc189072b6005af629d41129fe96e08
Alex Cote <apc51104@gmail.com>
2023-12-04 16:44:34 -0500

    Fix database connection and include login check

--- RPM/pages/post/create_listing.php
+++ RPM/pages/post/create_listing.php
@@ -1,94 +1,96 @@
 <?php
-// Connect to the database
+  // Connect to the database
 
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-session_start();
-include_once("../../database/connect.php");
-
-
-// //Connection to the database and its tables
-// $selectData = "SELECT JSON_OBJECT('listings', rpm_data.json) FROM RPM_data WHERE title = 'listings'";
-// $stmt = $pdo -> query($selectData);
-
-// $data = $stmt-> fetch(PDO::FETCH_ASSOC);
+  error_reporting(E_ALL);
+  ini_set('display_errors', 1);
+  session_start();
+  include_once("../../database/connect.php");
 
 
-if (isset($_POST["submit"]) && isset($_SESSION["rcs_id"])) { //Getting data from form
-  // validate the image
-  if (isset($_FILES['listingImage']) && $_FILES['listingImage']['error'] == UPLOAD_ERR_OK) {
-    $finfo = new finfo(FILEINFO_MIME_TYPE);
-    $mime = $finfo->file($_FILES['listingImage']['tmp_name']);
+  // //Connection to the database and its tables
+  // $selectData = "SELECT JSON_OBJECT('listings', rpm_data.json) FROM RPM_data WHERE title = 'listings'";
+  // $stmt = $pdo -> query($selectData);
 
-    if ($mime == 'image/png') {
-      // The uploaded file is a PNG image
+  // $data = $stmt-> fetch(PDO::FETCH_ASSOC);
 
-      // Check the file size
-      $size = $_FILES['listingImage']['size'];
-      $maxSize = 5000000; // Maximum file size in bytes (5MB)
 
-      if ($size <= $maxSize) {
-        // The file size is acceptable
-        // Move the uploaded file to the images directory
-        
-        
-        $dbDir = "/RPM/pages/marketplace/images/";
-        $targetDir = "../marketplace/images/";
-        $targetFile = $targetDir . basename($_FILES['listingImage']['name']);
-        $dbFile = $dbDir . basename($_FILES['listingImage']['name']);
+  if (isset($_POST["submit"]) && isset($_SESSION["rcs_id"])) { //Getting data from form
+    // validate the image
+    if (isset($_FILES['listingImage']) && $_FILES['listingImage']['error'] == UPLOAD_ERR_OK) {
+      $finfo = new finfo(FILEINFO_MIME_TYPE);
+      $mime = $finfo->file($_FILES['listingImage']['tmp_name']);
+
+      if ($mime == 'image/png') {
+        // The uploaded file is a PNG image
+
+        // Check the file size
+        $size = $_FILES['listingImage']['size'];
+        $maxSize = 5000000; // Maximum file size in bytes (5MB)
+
+        if ($size <= $maxSize) {
+          // The file size is acceptable
+          // Move the uploaded file to the images directory
           
-        if (move_uploaded_file($_FILES['listingImage']['tmp_name'], $targetFile)) {
-          // The file has been moved successfully
+          
+          $dbDir = "/RPM/pages/marketplace/images/";
+          $targetDir = "../marketplace/images/";
+          $targetFile = $targetDir . basename($_FILES['listingImage']['name']);
+          $dbFile = $dbDir . basename($_FILES['listingImage']['name']);
+            
+          if (move_uploaded_file($_FILES['listingImage']['tmp_name'], $targetFile)) {
+            // The file has been moved successfully
 
-          // Save the path to the image in the database
-          $listingImage = $dbFile;
+            // Save the path to the image in the database
+            $listingImage = $dbFile;
+          } else {
+            // header to index page with error
+            header("Location: index.html?error=Sorry, there was an error uploading your file.");
+          }
         } else {
-          // header to index page with error
-          header("Location: index.html?error=Sorry, there was an error uploading your file.");
+          // The file is too large
+          header("Location: index.html?error=Sorry, your file is too large.");
         }
       } else {
-        // The file is too large
-        header("Location: index.html?error=Sorry, your file is too large.");
+        // The uploaded file is not a PNG image
+        // echo "Please upload a PNG image.";
+        header("Location: index.html?error=Please upload a PNG image.");
       }
     } else {
-      // The uploaded file is not a PNG image
-      // echo "Please upload a PNG image.";
-      header("Location: index.html?error=Please upload a PNG image.");
+      // No file was uploaded, or there was an error
+      // echo "An error occurred during file upload.";
+      header("Location: index.html?error=An error occurred during file upload.");
     }
-  } else {
-    // No file was uploaded, or there was an error
-    // echo "An error occurred during file upload.";
-    header("Location: index.html?error=An error occurred during file upload.");
+
+
+    //Getting data from form 
+    $listingTitle = htmlspecialchars($_POST["listingTitle"], ENT_QUOTES, 'UTF-8');
+    $listingPrice = htmlspecialchars(round(floatval($_POST["listingPrice"]), 2), ENT_QUOTES, 'UTF-8');
+    $listingCondition = htmlspecialchars($_POST["listingCondition"], ENT_QUOTES, 'UTF-8');
+    $listingDescription = htmlspecialchars($_POST["listingDescription"], ENT_QUOTES, 'UTF-8');
+    $listingCategory = htmlspecialchars($_POST["listingCategory"], ENT_QUOTES, 'UTF-8');
+    $rcs_id = $_SESSION["rcs_id"];
+    date_default_timezone_set('America/New_York'); //Ensuring the date is all in the same timezone (NY)
+    $listingDate = date('Y-m-d');
+
+    //print data to check if working
+    // echo $listingTitle . " " . $listingPrice . " " . $listingCondition . " " . $listingDescription . " " . $listingImage . " " . $listingColor . " " . $rcs_id . " " . $listingDate;
+
+    //Inserting data into the database
+    $insertData = $pdo->prepare("INSERT INTO listings (rcs_id, item_condition, item_description, listing_title, posting_date, price, category, image_path) 
+      VALUES (:rcs_id, :listingCondition, :listingDescription, :listingTitle, :listingDate, :listingPrice, :listingCategory, :listingImage)");
+    $insertData->execute(array(
+      ':rcs_id' => $rcs_id,
+      ':listingCondition' => $listingCondition,
+      ':listingDescription' => $listingDescription,
+      ':listingTitle' => $listingTitle,
+      ':listingDate' => $listingDate,
+      ':listingPrice' => $listingPrice,
+      ':listingCategory' => $listingCategory,
+      ':listingImage' => $listingImage
+    ));
+
+
+    header("Location: index.html?success=Your listing has been created.");
   }
-
-
-  //Getting data from form 
-  $listingTitle = htmlspecialchars($_POST["listingTitle"], ENT_QUOTES, 'UTF-8');
-  $listingPrice = htmlspecialchars(round(floatval($_POST["listingPrice"]), 2), ENT_QUOTES, 'UTF-8');
-  $listingCondition = htmlspecialchars($_POST["listingCondition"], ENT_QUOTES, 'UTF-8');
-  $listingDescription = htmlspecialchars($_POST["listingDescription"], ENT_QUOTES, 'UTF-8');
-  $listingCategory = htmlspecialchars($_POST["listingCategory"], ENT_QUOTES, 'UTF-8');
-  $rcs_id = $_SESSION["rcs_id"];
-  date_default_timezone_set('America/New_York'); //Ensuring the date is all in the same timezone (NY)
-  $listingDate = date('Y-m-d');
-
-  //print data to check if working
-  // echo $listingTitle . " " . $listingPrice . " " . $listingCondition . " " . $listingDescription . " " . $listingImage . " " . $listingColor . " " . $rcs_id . " " . $listingDate;
-
-  //Inserting data into the database
-  $insertData = $pdo->prepare("INSERT INTO listings (rcs_id, item_condition, item_description, listing_title, posting_date, price, category, image_path) 
-    VALUES (:rcs_id, :listingCondition, :listingDescription, :listingTitle, :listingDate, :listingPrice, :listingCategory, :listingImage)");
-  $insertData->execute(array(
-    ':rcs_id' => $rcs_id,
-    ':listingCondition' => $listingCondition,
-    ':listingDescription' => $listingDescription,
-    ':listingTitle' => $listingTitle,
-    ':listingDate' => $listingDate,
-    ':listingPrice' => $listingPrice,
-    ':listingCategory' => $listingCategory,
-    ':listingImage' => $listingImage
-  ));
-
-
-  header("Location: index.html?success=Your listing has been created.");
-}
+  header("Location: ../user/login/index.html?error=You must be logged in to create a listing.");
+?>
