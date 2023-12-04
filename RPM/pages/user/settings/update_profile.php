2320eddf12b7602abe38321fafda4ab99873d0df
Alex Cote <apc51104@gmail.com>
2023-12-03 22:41:01 -0500

    Fix include path in update_profile.php

--- RPM/pages/user/settings/update_profile.php
+++ RPM/pages/user/settings/update_profile.php
@@ -6,7 +6,7 @@
   error_reporting(E_ALL);
   ini_set('display_errors', 1);
   session_start();
-  include_once("../../database/connect.php");
+  include_once("/RPM/database/connect.php");
 
 
   // //Connection to the database and its tables
