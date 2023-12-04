8fe89fc4f61cfb706f4bc78966967e786c0abed1
Alex Cote <apc51104@gmail.com>
2023-12-04 00:11:47 -0500

    Fix category comparison in get_listings.php

--- RPM/pages/marketplace/get_listings.php
+++ RPM/pages/marketplace/get_listings.php
@@ -4,12 +4,16 @@
   session_start();
   include_once("../../database/connect.php");
   include_once("../../database/check_login.php");
+  
+  $category = isset($_GET['category']) ? $_GET['category'] : 'All';
 
-  if($_GET['category'] == "ALL"){
+  if($category == "All"){
     $stmt = $pdo->prepare("SELECT * FROM listings ORDER BY price ASC");
+    echo "All";
   }else{
     $stmt = $pdo->prepare("SELECT * FROM listings WHERE category = :category ORDER BY price ASC");
     $stmt->bindValue(':category', $_GET['category']);
+    echo "Not All";
   }
   // $stmt = $pdo->prepare("SELECT * FROM listings ORDER BY price ASC"); //Top listings are the cheaper ones or free
   $stmt->execute();
